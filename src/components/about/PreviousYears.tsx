ec0ab89aacc24a7a5c61469134459b5762542afe
xwmtp <xwillmtp@gmail.com>
2022-05-24 20:42:45 +0200

    Change order previous year blocks

--- src/components/about/PreviousYears.tsx
+++ src/components/about/PreviousYears.tsx
@@ -6,14 +6,14 @@ export const PreviousYears: React.FC = () => {
   return (
     <>
       <ImageBlockBottomMargin
-        text={"2020"}
-        url={"https://xwmtp.github.io/bingo2020/"}
-        image={"https://nextcloud.scaramangado.de/s/2QJMtmxoDJR55Pk/preview"}
-      />
-      <ImageBlock
         text={"2021"}
         url={"https://xwmtp.github.io/bingo2021/"}
         image={"https://nextcloud.scaramangado.de/s/9SgC5w4szkEjsEg/preview"}
+      />
+      <ImageBlock
+        text={"2020"}
+        url={"https://xwmtp.github.io/bingo2020/"}
+        image={"https://nextcloud.scaramangado.de/s/2QJMtmxoDJR55Pk/preview"}
       />
     </>
   );
