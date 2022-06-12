bdbfb68e2a1490669cee2821ea5f3e4d3c7e63bc
xwmtp <xwillmtp@gmail.com>
2022-06-12 18:33:33 +0200

    Add rules to AboutPage

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import styled from "styled-components";
 import { Margins } from "../GlobalStyle";
 import { PreviousYears } from "../components/pages/about/PreviousYears";
+import { ExternalLink } from "../components/general/ExternalLink";
+import { tournamentSettings } from "../TournamentSetings";
+import { InternalLink } from "../components/general/InternalLink";
 
 export const AboutPage: React.FC = () => {
   //const aboutWidth = 700 - Math.round(Margins.container / 2);
@@ -11,13 +14,157 @@ export const AboutPage: React.FC = () => {
   return (
     <AboutPageDiv>
       <Container title={"About"} width={"70%"}>
-        <p> Welcome to the 2022 OoT Bingo Tournament!</p>
+        <AboutContent>
+          <p> Welcome to the 2022 Ocarina of Time Bingo Tournament!</p>
+          <br />
 
-        <p>
-          During this stage, every week, each player will be paired with another player for a bingo
-          match. This pairing is calculated according to the points you have gathered so far,
-          meaning you will play against people with similar scores. There will be 4 of these rounds.
-        </p>
+          <h3>General info</h3>
+          <List>
+            <ListItem>The tournament will start on …</ListItem>
+            <ListItem>
+              This is a regular{" "}
+              <ExternalLink url="https://ootbingo.github.io/bingo/latest-version.html">
+                Bingo tournament
+              </ExternalLink>
+              , played on the latest version (currently <strong>v10.1</strong>). Note that the
+              version may be updated between rounds, but never while a round is in progress.
+            </ListItem>
+            <ListItem>
+              The tournament consists of two phases: a swiss-round phase followed by a
+              single-elimination bracket phase.
+            </ListItem>
+            <ListItem>
+              Having a <ExternalLink url="https://racetime.gg/${}">Racetime.gg</ExternalLink>{" "}
+              account with at least one race in the OoT bingo category is required to join the
+              tournament. You may sign up before you completed one race, but should finish one
+              before the tournament starts.
+            </ListItem>
+          </List>
+
+          <h3>Rules</h3>
+          <List>
+            <ListItem>
+              All races must be played on{" "}
+              <ExternalLink url={`https://racetime.gg/${tournamentSettings.RACETIME_CATEGORY}`}>
+                Racetime.gg
+              </ExternalLink>{" "}
+              and announced when about to start.
+            </ListItem>
+            <ListItem>
+              <strong>ESNB</strong> and <strong>SNB</strong> may not be used for any match in the
+              tournament. All races have to be played in a different room from <strong>ESNB</strong>{" "}
+              or <strong>SNB</strong>
+            </ListItem>
+            <ListItem>
+              It is not allowed to help entrants with their bingo during tournament races.
+            </ListItem>
+            <ListItem>
+              Entrants may not watch the restream of their match while they are racing.
+            </ListItem>
+            <ListItem>All races must be livestreamed.</ListItem>
+          </List>
+
+          <h3>Scheduling & recording</h3>
+          <List>
+            <ListItem>
+              After you've been assigned an opponent, you will find your match on your{" "}
+              <InternalLink to="/profile/matches">profile page</InternalLink>. You can schedule your
+              race by pressing the 'Pick time' button. Only one match entrant has to do so. Please
+              only schedule after you agreed on a time with your opponent.
+            </ListItem>
+            <ListItem>
+              You can record the results of the match to the website after the Racetime race has
+              been <strong>recorded</strong>. Copy the url and click the ''.
+              <InternalLink to="/profile/matches">profile page</InternalLink>. You can schedule your
+              race by pressing the 'Pick time' button. Only one match entrant has to do so. Please
+              only schedule after you agreed on a time with your opponent.
+            </ListItem>
+            <ListItem>
+              If, for any reason, you are unable to to play your match, please contact a Tournament
+              Organizer.
+            </ListItem>
+          </List>
+
+          <h3>Phase 1: Swiss Rounds</h3>
+          <List>
+            <ListItem>
+              The first phase consists of <strong>4</strong> rounds.
+            </ListItem>
+            <ListItem>Each round starts on … and lasts exactly one week.</ListItem>
+            <ListItem>
+              All races have to be finished before the end of the round. If a race is not done in
+              time, the decision about the result of the match and further action regarding both
+              players is left to the tournament organizers and will be treated case by case.
+            </ListItem>
+            <ListItem>
+              A win is awarded <strong>3</strong> points. In the unlikely case of a draw, both
+              players are awarded <strong>1</strong> point.
+            </ListItem>
+            <ListItem>
+              A forfeit is counted as a <strong>4:00:00</strong> time.
+            </ListItem>
+            <ListItem>
+              Ranking is based on amount of points, then median time in the tournament, then amount
+              of finished (not forfeited) races in the tournament.
+            </ListItem>
+
+            <br />
+            <strong>Seeding and paring</strong>
+            <List>
+              <ListItem>
+                Seeding and pairing will be based on the{" "}
+                <ExternalLink url="https://xwmtp.github.io/bingo-leaderboard">
+                  Bingo leaderboard
+                </ExternalLink>{" "}
+                rating, not the tournament's ranking.
+              </ListItem>
+              <ListItem>
+                Before Round 1 starts, the upper half (rounded up) of the leaderboard gets one{" "}
+                <em>virtual point</em>. The virtual point is removed before Round 3 starts.
+              </ListItem>
+              <ListItem>
+                Within each group of entrants with the same amount of points (including the virtual
+                point), the highest-seeded player will face the lowest-seeded player, the second
+                highest will face the second lowest, and so on.
+              </ListItem>
+              <ListItem>
+                If a group has an odd number of entrants, the highest seed of that group floats down
+                to the group below. Each entrant can be floated down according to that rule at most
+                once.
+              </ListItem>
+              <ListItem>
+                If there is an odd number of entrants, the highest seed of the lowest group at each
+                round gets a <strong>bye</strong>. A maximum of one bye can be given to each
+                entrant.
+              </ListItem>
+              <ListItem>
+                A player that received a bye should still do a race that Round against any
+                opponent(s), to set a a time for their tournament median. If not, they receive a
+                forfeit time (but still 3 points).
+              </ListItem>
+            </List>
+            <ListItem>
+              At the end of the 4th round, the top <strong>13</strong> players will advance to the
+              second phase as seeds 1 to 13.
+            </ListItem>
+            <ListItem>
+              Among the remaining players, the top <strong>3</strong> median times will advance to
+              the second phase as seeds 14 to 16.
+            </ListItem>
+          </List>
+
+          <h3>Phase 2: Bracket</h3>
+          <List>
+            <ListItem>
+              The second phase is a <strong>single-elimination</strong> bracket.
+            </ListItem>
+            <ListItem>In the event of a draw, the match has to be played again.</ListItem>{" "}
+            <ListItem>
+              Each match consists of one race, except for the grand finals, which is played in three
+              races.
+            </ListItem>
+          </List>
+        </AboutContent>
       </Container>
       <PreviousYearsContainer title={"Previous years"} width={"30%"}>
         <PreviousYears />
@@ -36,4 +183,16 @@ const AboutPageDiv = styled.div`
 
 const PreviousYearsContainer = styled(Container)`
   margin-left: ${Margins.container}rem;
+`;
+
+const AboutContent = styled.div`
+  line-height: 1.3rem;
+`;
+
+const List = styled.ul`
+  padding: 1rem 1.5rem;
+`;
+
+const ListItem = styled.li`
+  padding: 0.15rem 0;
 `;
