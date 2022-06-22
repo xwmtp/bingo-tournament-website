ad1e7b0fa4dfef8bad959131f9d1dec9ad496ce2
xwmtp <xwillmtp@gmail.com>
2022-06-22 15:12:40 +0200

    Use production backend url

--- src/Settings.ts
+++ src/Settings.ts
@@ -7,5 +7,7 @@ export const websiteSettings = {
   DEFAULT_AVATAR:
     "https://github.com/xwmtp/bingo2022/blob/assets/images/avatars/neutralAvatar.png?raw=true",
   LOGIN_URL: "https://bingo-tournament.scaramangado.de/login/racetime",
-  //LOGIN_URL: "localhost:8080/login/racetime",
+  BACKEND_URL: "https://bingo-tournament.scaramangado.de/api",
+  //LOGIN_URL: "localhost:8000/login/racetime",
+  //BACKEND_URL: undefined,
 } as const;
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,10 @@
 import { Configuration, DefaultApi } from "@xwmtp/bingo-tournament";
+import { websiteSettings } from "../Settings";
 
 export const getApi: () => DefaultApi = () => {
   return new DefaultApi(
     new Configuration({
+      basePath: websiteSettings.BACKEND_URL,
       credentials: "include",
       headers: {
         "X-XSRF-TOKEN": document.cookie.replace(
