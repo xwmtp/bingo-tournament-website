a85b564fe01d7226d1141fb6181167e5701eed59
xwmtp <xwillmtp@gmail.com>
2022-06-22 15:04:38 +0200

    Use production login url

--- src/Settings.ts
+++ src/Settings.ts
@@ -6,6 +6,6 @@ export const tournamentSettings = {
 export const websiteSettings = {
   DEFAULT_AVATAR:
     "https://github.com/xwmtp/bingo2022/blob/assets/images/avatars/neutralAvatar.png?raw=true",
-  //LOGIN_URL: "https://bingo-tournament.scaramangado.de/login/racetime",
-  LOGIN_URL: "localhost:8080/login/racetime",
+  LOGIN_URL: "https://bingo-tournament.scaramangado.de/login/racetime",
+  //LOGIN_URL: "localhost:8080/login/racetime",
 } as const;
