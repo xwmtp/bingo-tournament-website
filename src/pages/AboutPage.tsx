c2a872ba4f7e70d4fa4a8c6edbe88fb7e3d85ef4
xwmtp <xwillmtp@gmail.com>
2022-06-30 23:15:05 +0200

    Update about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -6,6 +6,7 @@ import { PreviousYears } from "../components/pages/about/PreviousYears";
 import { ExternalLink } from "../components/general/ExternalLink";
 import { tournamentSettings } from "../Settings";
 import { InternalLink } from "../components/general/InternalLink";
+import { BiCalendar, BiSave } from "react-icons/bi";
 
 export const AboutPage: React.FC = () => {
   return (
@@ -17,14 +18,20 @@ export const AboutPage: React.FC = () => {
 
           <h3>General info</h3>
           <List>
-            <ListItem>The tournament will start on …</ListItem>
+            <ListItem>
+              The tournament will start on <strong>Monday, July 18th.</strong>
+            </ListItem>
+            <ListItem>
+              Sign up will close on <strong>Friday, July 15th, 23:59 UTC.</strong>
+            </ListItem>
             <ListItem>
               This is a regular{" "}
               <ExternalLink url="https://ootbingo.github.io/bingo/latest-version.html">
                 Bingo
               </ExternalLink>{" "}
               tournament, played on the latest version (currently <strong>v10.1</strong>). Note that
-              the version may be updated between rounds, but never while a round is in progress.
+              the version <em>may</em> be updated between rounds, but never while a round is in
+              progress.
             </ListItem>
             <ListItem>
               The tournament consists of two phases: a swiss-round phase followed by a
@@ -32,9 +39,9 @@ export const AboutPage: React.FC = () => {
             </ListItem>
             <ListItem>
               Having a <ExternalLink url="https://racetime.gg/${}">Racetime.gg</ExternalLink>{" "}
-              account with at least one race in the OoT bingo category is required to join the
-              tournament. You may sign up before you completed one race, but should finish one
-              before the tournament starts.
+              account with <em>at least one</em> race in the OoT Bingo category is required to join
+              the tournament. You may sign up before you completed one race, but should finish one
+              before the tournament starts (join ESNB/SNB for example!).
             </ListItem>
           </List>
 
@@ -45,12 +52,12 @@ export const AboutPage: React.FC = () => {
               <ExternalLink url={`https://racetime.gg/${tournamentSettings.RACETIME_CATEGORY}`}>
                 Racetime.gg
               </ExternalLink>{" "}
-              and announced when about to start.
+              and announced in the Discord when about to start.
             </ListItem>
             <ListItem>
               <strong>ESNB</strong> and <strong>SNB</strong> may not be used for any match in the
               tournament. All races have to be played in a different room from <strong>ESNB</strong>{" "}
-              or <strong>SNB</strong>
+              or <strong>SNB</strong>.
             </ListItem>
             <ListItem>
               It is not allowed to help entrants with their bingo during tournament races.
@@ -66,15 +73,14 @@ export const AboutPage: React.FC = () => {
             <ListItem>
               After you've been assigned an opponent, you will find your match on your{" "}
               <InternalLink to="/profile/matches">profile page</InternalLink>. You can schedule your
-              race by pressing the 'Pick time' button. Only one match entrant has to do so. Please
-              only schedule after you agreed on a time with your opponent.
+              race by pressing the <CalendarIcon /> 'pick time' button. Only one match entrant has
+              to do so. Only schedule after you agreed on a time with your opponent.
             </ListItem>
             <ListItem>
-              You can record the results of the match to the website after the Racetime race has
-              been <strong>recorded</strong>. Copy the url and click the ''.
-              <InternalLink to="/profile/matches">profile page</InternalLink>. You can schedule your
-              race by pressing the 'Pick time' button. Only one match entrant has to do so. Please
-              only schedule after you agreed on a time with your opponent.
+              After the Racetime race has been <strong>recorded</strong>, you can save the results
+              of your match to the website. Copy the Racetime race url, go to your{" "}
+              <InternalLink to="/profile/matches">profile page</InternalLink>, and click the
+              <SaveIcon /> button.
             </ListItem>
             <ListItem>
               If, for any reason, you are unable to to play your match, please contact a Tournament
@@ -87,18 +93,20 @@ export const AboutPage: React.FC = () => {
             <ListItem>
               The first phase consists of <strong>4</strong> rounds.
             </ListItem>
-            <ListItem>Each round starts on … and lasts exactly one week.</ListItem>
+            <ListItem>
+              Each round starts on <strong>Monday, 4pm UTC</strong> and lasts exactly one week.
+            </ListItem>
             <ListItem>
               All races have to be finished before the end of the round. If a race is not done in
               time, the decision about the result of the match and further action regarding both
               players is left to the tournament organizers and will be treated case by case.
             </ListItem>
             <ListItem>
-              A win is awarded <strong>3</strong> points. In the unlikely case of a draw, both
-              players are awarded <strong>1</strong> point.
+              A win is awarded <strong>3</strong> points. In the unlikely case of a draw (to the
+              second), both players are awarded <strong>1</strong> point.
             </ListItem>
             <ListItem>
-              A forfeit is counted as a <strong>4:00:00</strong> time.
+              A forfeit is counted as a <strong>4:00:00</strong> time for the tournament median.
             </ListItem>
             <ListItem>
               Ranking is based on amount of points, then median time in the tournament, then amount
@@ -192,4 +200,16 @@ const List = styled.ul`
 
 const ListItem = styled.li`
   padding: 0.15rem 0;
+`;
+
+const SaveIcon = styled(BiSave)`
+  margin-left: 0.4rem;
+  margin-right: 0.2rem;
+  transform: scale(1.3);
+`;
+
+const CalendarIcon = styled(BiCalendar)`
+  margin-left: 0.3rem;
+  margin-right: 0.2rem;
+  transform: scale(1.2);
 `;
